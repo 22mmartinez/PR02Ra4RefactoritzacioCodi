d62522dfe4ad62a52e513d4319a11fbba2e3b8ab
22mmartinez <22mmartinez@ibadia.cat>
2026-06-03 00:17:34 +0200

    Refactor: Eliminat el codi heretat anidat i tancat el cas d'articles normals

--- src/Magatzem.java
+++ src/Magatzem.java
@@ -9,12 +9,12 @@ class Magatzem {
         for (int i = 0; i < articles.length; i++) {
             Article article = articles[i];
 
-            // 1. Cas Llegendari
+            // 1. Martell de Thor (Llegendari)
             if (article.nom.equals("Martell de Thor (Llegendari)")) {
                 continue;
             }
 
-            // 2. Cas Formatge Gidurat
+            // 2. Formatge Gidurat
             if (article.nom.equals("Formatge Gidurat")) {
                 article.diesPerVendre--;
                 if (article.qualitat < 50) {
@@ -26,76 +26,32 @@ class Magatzem {
                 continue;
             }
 
-            //  NUEVO: Cas Entrades per al Concert (Limpio y ordenado)
+            // 3. Entrades per al Concert
             if (article.nom.equals("Entrades per al Concert del Trobador")) {
                 article.diesPerVendre--;
-
-                // Si el concierto ya ha pasado, la calidad cae a cero de golpe
                 if (article.diesPerVendre < 0) {
                     article.qualitat = 0;
-                    continue; // 🚀 ¡EXPULSIÓN!
+                    continue;
                 }
-
                 if (article.qualitat < 50) {
-                    article.qualitat++; // Subida estándar (+1) si quedan más de 10 días
-
+                    article.qualitat++;
                     if (article.diesPerVendre < 10 && article.qualitat < 50) {
-                        article.qualitat++; // Entre 10 y 6 días (un punto extra, total +2)
+                        article.qualitat++;
                     }
                     if (article.diesPerVendre < 5 && article.qualitat < 50) {
-                        article.qualitat++; // 5 días o menos (otro punto extra, total +3)
+                        article.qualitat++;
                     }
                 }
-                continue; // 🚀 ¡EXPULSIÓN!
+                continue;
             }
 
-            if (!articles[i].nom.equals("Formatge Gidurat")
-                    && !articles[i].nom.equals("Entrades per al Concert del Trobador")) {
-                if (articles[i].qualitat > 0) {
-                    if (!articles[i].nom.equals("Martell de Thor (Llegendari)")) {
-                        articles[i].qualitat = articles[i].qualitat - 1;
-                    }
-                }
-            } else {
-                if (articles[i].qualitat < 50) {
-                    articles[i].qualitat = articles[i].qualitat + 1;
-
-                    if (articles[i].nom.equals("Entrades per al Concert del Trobador")) {
-                        if (articles[i].diesPerVendre < 11) {
-                            if (articles[i].qualitat < 50) {
-                                articles[i].qualitat = articles[i].qualitat + 1;
-                            }
-                        }
-
-                        if (articles[i].diesPerVendre < 6) {
-                            if (articles[i].qualitat < 50) {
-                                articles[i].qualitat = articles[i].qualitat + 1;
-                            }
-                        }
-                    }
-                }
+            // 4. Articles Normals (Només arriba aquí el que no s'ha interceptat dalt)
+            article.diesPerVendre--;
+            if (article.qualitat > 0) {
+                article.qualitat--;
             }
-
-            if (!articles[i].nom.equals("Martell de Thor (Llegendari)")) {
-                articles[i].diesPerVendre = articles[i].diesPerVendre - 1;
-            }
-
-            if (articles[i].diesPerVendre < 0) {
-                if (!articles[i].nom.equals("Formatge Gidurat")) {
-                    if (!articles[i].nom.equals("Entrades per al Concert del Trobador")) {
-                        if (articles[i].qualitat > 0) {
-                            if (!articles[i].nom.equals("Martell de Thor (Llegendari)")) {
-                                articles[i].qualitat = articles[i].qualitat - 1;
-                            }
-                        }
-                    } else {
-                        articles[i].qualitat = articles[i].qualitat - articles[i].qualitat;
-                    }
-                } else {
-                    if (articles[i].qualitat < 50) {
-                        articles[i].qualitat = articles[i].qualitat + 1;
-                    }
-                }
+            if (article.diesPerVendre < 0 && article.qualitat > 0) {
+                article.qualitat--; // Baixa el doble de ràpid si ha caducat
             }
         }
     }
